c6f21dd4917cbdbd8ee2ede892c8b3ea41d883cc
BaiBlanc <33418651+BaiBlanc@users.noreply.github.com>
2019-04-02 22:08:01 +0200

    Little progress
    .jpeg.json => .json
    
    Don't read json as image

--- imageReader.js
+++ imageReader.js
@@ -6,40 +6,47 @@ const parameters = require("./parameters.json")
 const { InputFolders } = parameters
 Object.keys(InputFolders).forEach(type => {
     const folder = InputFolders[type]
-    fs.readdir(folder, (err, files)=>{
-        if(err){
+    fs.readdir(folder, (err, files) => {
+        if (err) {
             console.error(err)
-        }else{
-            files.map(file=>{
-                // Read the image
-                const image = fs.readFileSync(folder+file)
+        } else {
+            files.map(file => {
+                if (file.includes(".json")) {
+                    // Do nothing with the JSON file
+                } else { // Read the image
 
-                // Builde the parameters of Rekogntion
-                const param = {
-                    Image: {
-                        //    No need to pass to Base64, 
-                        //    because it's aotomatically transfered to Base64 after the tag 'Bytes'
-                        Bytes: image
-                    },
-                    MaxLabels: 10,
-                    MinConfidence: 77
-                }
-                // Using rekognition APIs for image analysis
-                rekognition.detectLabels(param, function (err, data) {
-                    if (err) {
-                        console.log(err)
-                    } else {
-                        console.log(data)
-                        var stringData = JSON.stringify(data)
-                        stringData = stringData.replace(/,/g, ",\n")
-                        fs.writeFile(file+".json", stringData, (err)=>{
-                            if(err){
-                                console.error(err)
-                            }
-                        })
-                        // Upload object to S3
+                    const image = fs.readFileSync(folder + file)
+
+                    // Builde the parameters of Rekogntion
+                    const param = {
+                        Image: {
+                            //    No need to pass to Base64, 
+                            //    because it's aotomatically transfered to Base64 after the tag 'Bytes'
+                            Bytes: image
+                        },
+                        MaxLabels: 10,
+                        MinConfidence: 77
                     }
-                })
+                    // Using rekognition APIs for image analysis
+                    rekognition.detectLabels(param, function (err, data) {
+                        if (err) {
+                            console.log(err)
+                        } else {
+                            console.info("Result of "+file)
+                            console.log(data)
+                            var stringData = JSON.stringify(data)
+                            stringData = stringData.replace(/,/g, ",\n")
+                            const fileName = file.replace(/.jpeg/, ".json")
+                            fs.writeFile(folder + fileName, stringData, (err) => {
+                                if (err) {
+                                    console.error(err)
+                                }
+                            })
+                            // Upload object to S3
+                        }
+                    })
+                }
+
             })
         }
     })
